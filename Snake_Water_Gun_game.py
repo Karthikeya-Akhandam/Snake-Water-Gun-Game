7536dceae8d6250c67db18fcc43d8fea23d13c8b
Karthikeya-Akhandam <191661911+Karthikeya-Akhandam@users.noreply.github.com>
2025-02-21 22:39:22 +0530

    Added main file

---
+++ Snake_Water_Gun_game.py
@@ -0,0 +1,16 @@
+import random
+def option(opt_user, opt_bot) :
+    if (opt_user == 1 and opt_bot == 2) :
+        return "You"
+    elif (opt_user == 2 and opt_bot == 3) :
+        return "You"
+    elif (opt_user == 3 and opt_bot == 1) :
+        return "You"
+    elif (opt_bot == opt_user) :
+        print("Draw!")
+    else :
+        return "Bot"
+opt_bot = random.randint(1,3)
+opt_user = int(input("1. Snake\n2. Water\n3.Gun\nEnter your choice: "))
+print("Bot chose: ",opt_bot)
+print("Round won by: ",option(opt_user,opt_bot))
